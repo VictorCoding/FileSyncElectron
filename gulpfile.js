1e7a4e8097cec58563226650083f43f35cc0a361
VictorCoding <victor.ramos1@gmail.com>
2015-11-21 11:06:42 -0600

    adding angular material add new npm packages to package.json update gitignore add angular-material to bower.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,30 @@ var gulp = require("gulp");
 var babel = require("gulp-babel");
 var sourcemaps = require("gulp-sourcemaps");
 var concat = require("gulp-concat");
+var clean = require("gulp-clean");
+var watch = require("gulp-watch");
+var runSequence = require("run-sequence");
 
-gulp.task("default", function () {
-  return gulp.src("app/**/*.js")
+gulp.task("default", function(){
+  runSequence("clean-bundle", "build", "watch");
+});
+
+gulp.task("build", function () {
+  return gulp.src(["app.js", "app/**/*.js", "!bundle.js"])    
     .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(concat("all.js"))
+    .pipe(concat("bundle.js"))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("app/Scripts"));
+    .pipe(gulp.dest("app"));    
+});
+
+gulp.task("clean-bundle", function () {
+  return gulp.src(["app/bundle.js", "app/bundle.js.map"], {read: false})
+    .pipe(clean());
+});
+
+gulp.task("watch", function(){
+  gulp.watch(["app/**/*.js", "!bundle.js", "!bundle.js.map"], function(){
+    runSequence("clean-bundle", "build");
+  });
 });
