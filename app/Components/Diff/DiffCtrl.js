5a749f0f078119d71d19f0e0a0b6fc32b13184a8
VictorCoding <victor.ramos1@gmail.com>
2015-11-28 15:12:49 -0600

    Diff feature to check files sync added.
    -Add angular controller, service, and template
    -Add JsDiff lib dependency

--- app/Components/Diff/DiffCtrl.js
+++ app/Components/Diff/DiffCtrl.js
@@ -1,10 +1,80 @@
-class DiffController {
-  contructor(FileDiff) {
-    angular.extend(this, {
-      FileDiff
-    });        
-  }
+angular
+      .module('FileSync')
+      .controller('DiffCtrl', DiffController);
+      
+function DiffController(FileDiff) {
+  let self = this;
   
+  self.languages = [
+    {
+      lang: 'English',
+      json: '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/en_US.json',
+      prop: '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_en_US.properties'
+    },
+    {
+      lang: 'Spanish',
+      json: '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/es_ES.json',
+      prop: '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_es.properties'
+    },
+    {
+      lang: 'Portuguese',
+      json: '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/pt_BR.json',
+      prop: '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_pt.properties'
+    },
+    {
+      lang: 'Default',
+      json: '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/default.json',
+      prop: '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect.properties'
+    },
+    {
+      lang: 'German',
+      json: '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/de_DE.json',
+      prop: '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_de.properties'
+    },
+    {
+      lang: 'Swedish',
+      json: '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/sv_SE.json',
+      prop: '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_sv.properties'
+    },
+    {
+      lang: 'Japanese',
+      json: '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/ja_JP.json',
+      prop: '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_ja.properties'
+    },
+    {
+      lang: 'Chinese',
+      json: '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/zh_CN.json',
+      prop: '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_zh.properties'
+    },
+    {
+      lang: 'French',
+      json: '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/fr_FR.json',
+      prop: '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_fr.properties'
+    },
+    {
+      lang: 'UK',
+      json: '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/en_GB.json',
+      prop: '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_en_GB.properties'
+    }
+  ];
+                      
+  self.showDiff = function showDiff(lang) {
+    var chosenLang = _.filter(this.languages, (l)=> {
+      return l.lang == lang.lang;
+    })[0];
+      
+    var keys = FileDiff.getDiff(chosenLang.json, chosenLang.prop);
+                                       
+    self.propKeys = keys.propKeys;
+    self.jsonKeys = keys.jsonKeys;
+    self.keysDiff = _.map(keys.keysDiff, (text)=> {
+      var color = text.removed ? 'red' : text.added ? 'green' : 'lightgrey';
+      return {
+        text: text.value.trim(),
+        color,
+      };
+    });
+  };
+  
+  self.showDiff(this.languages[0]);
 }
-
-angular.module('FileSync').controller('DiffCtrl', DiffController);
--- app/Services/FileDiff.js
+++ app/Services/FileDiff.js
@@ -1,14 +1,72 @@
+const fs = require('fs');
+
 class FileDiff {
   constructor(firebaseDB) {
     angular.extend(this, {
       firebaseDB,
     });
+    this.files = ['/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect.properties',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_de.properties',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_en_GB.properties',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_en_US.properties',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_es.properties',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_fr.properties',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_ja.properties',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_pt.properties',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_sv.properties',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/resources/iOfficeConnect_zh.properties',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/de_DE.json',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/default.json',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/en_GB.json',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/en_US.json',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/es_ES.json',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/fr_FR.json',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/ja_JP.json',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/pt_BR.json',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/sv_SE.json',
+                  '/Users/vramos/Workspace/iOfficeConnect/src/main/webapp/include/javascript/i18n/zh_CN.json'];
     
-    
-    this.firebaseDB.loadFiles().then((f) => this.files = f);
+    // this.firebaseDB.loadFiles().then((f) => this.files = f);
   }
 
-  getDiff() {
+  getDiff(jsonFile, propFile) {
+    let jsonContent = fs.readFileSync(jsonFile, 'utf8');
+    let propContent = fs.readFileSync(propFile, 'utf8');
+    let jsonKeys = this._getKeys(jsonContent, 'json');
+    let propKeys = this._getKeys(propContent);
+
+    var keysDiff = this._compare(propKeys, jsonKeys);
+    
+    return {
+      jsonKeys,
+      propKeys,
+      keysDiff,
+    };
+  }
+  
+  _getKeys(content, type) {
+    if(type === 'json'){
+      let pairsArr = content.split('\n');
+      pairsArr.pop();
+      pairsArr.shift();
+      
+      return _.map(pairsArr, (pair)=> {
+        return pair.split(':')[0].split('"')[1];
+      });
+    } else {
+      let pairsArr = content.split('\n');
+      
+      return _.map(pairsArr, (pair)=> {
+        return pair.split('=')[0];
+      });
+    }
+  }
+  
+  _compare(arr1, arr2) {
+    var jsonTxtBlock = arr1.join('\n');
+    var propTxtBlock = arr2.join('\n');
+    
+    return JsDiff.diffWords(jsonTxtBlock, propTxtBlock);
     
   }
 }
