ae34970d4923d78e64d663f50f46ab99ced85b65
VictorCoding <victor.ramos1@gmail.com>
2015-12-27 18:52:30 -0600

    Add mdDialog to home view. If user hasn't set a path to either json or properties files, dialog should show up advicing to do so

--- app/Components/Home/HomeController.js
+++ app/Components/Home/HomeController.js
@@ -1,7 +1,32 @@
 class HomeCtrl {
-  constructor() {
-    
+  constructor($mdDialog, $state) {
+    this.$mdDialog = $mdDialog;
+    this.$state = $state;
+    this.checkLocalizationFilesSettings();
   }    
+  
+  checkLocalizationFilesSettings(ev) {
+    var content = fs.readFileSync('./settings.json', 'utf8');
+    content = JSON.parse(content);
+    
+    if (!content.jsonPath || !content.propertiesPath) {
+      this._showAlert();
+    }
+    
+  }
+  
+  _showAlert() {
+    var alert = this.$mdDialog.alert({
+      title: 'Please set path to localization files',
+      ok: 'Ok'
+    });        
+          
+    this.$mdDialog
+        .show(alert)
+        .finally(()=> {
+          this.$state.go('settings');
+        });
+  }
 }
 
 angular.module('FileSync').controller('HomeCtrl', HomeCtrl);
